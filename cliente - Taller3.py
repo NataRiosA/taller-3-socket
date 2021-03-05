750cd2cee3eedd71519297440a67d638c236a116
Natalia <nataliarios325@gmail.com>
2021-03-05 17:45:43 -0500

    first commit

---
+++ cliente - Taller3.py
@@ -0,0 +1,51 @@
+import socket
+import os  
+# Cliente Taller 3: operaciones matemáticas con servidor intermedio
+
+
+print ("taller 3")
+host="localhost"
+puerto=9999
+socket1=socket.socket()
+socket1.connect((host,puerto))
+
+def menu():
+    print("**********MENU DE OPERACIONES*******************")
+    print("1. SUMA")
+    print("2. RESTA")
+    print("3. MULTIPLICACION")
+    print("4. DIVIDICION")
+    print("5. POTENCIACION")
+    print("6. LOGARITMACION")
+    print("0. PARA SALIR")
+
+menu()    
+   
+
+def digitarNumero():
+    x=(input("ingrese el primer numero: "))
+    y=(input("Ingrese el segundo numero: "))
+    socket1.send(x.encode("UTF-8"))
+    socket1.send(y.encode("UTF-8"))
+
+while True:
+    p=(input("Digite la operacion a realizar: "))
+   
+    if p=="1":
+        digitarNumero()               
+        socket1.send(p.encode("UTF-8"))
+        suman=socket1.recv(1024).decode("UTF-8")
+        print ("La suma es: ", suman)
+
+    if p=="2":
+       digitarNumero()
+       socket1.send(p.encode("UTF-8"))
+       restan=socket1.recv(1024).decode("UTF-8")
+       print("La resta es: ", restan)
+        
+    elif p=="0":
+        print("que tengas un buen dia, hasta pronto")
+        break
+"""     else:
+        os.system('cls') """
+     
---
+++ servidorInter - Taller3.py
@@ -0,0 +1,62 @@
+import socketserver
+import socket
+## Python 3.7
+
+puerto1=9997
+host1 = "localhost"
+socket1=socket.socket()
+socket1.connect((host1,puerto1))
+
+puerto2=9996
+socket2=socket.socket()
+socket2.connect((host1,puerto2))
+
+def MenuOperacion(x,y,op):
+	print(x,y,op)
+
+	if op=='1':
+		socket1.send(x.encode("UTF-8"))
+		socket1.send(y.encode("UTF-8"))
+		socket1.send(op.encode("UTF-8"))
+	
+	elif op=='2':
+		socket2.send(x.encode("UTF-8"))
+		socket2.send(y.encode("UTF-8"))
+		socket2.send(op.encode("UTF-8"))
+    
+	else: pass
+
+class miHandler(socketserver.BaseRequestHandler):
+
+
+	def handle(self):
+		self.numero1=str(self.request.recv(1024).decode("UTF-8"))
+		self.numero2=str(self.request.recv(1024).decode("UTF-8"))
+		self.operacion=str(self.request.recv(1024).decode("UTF-8"))
+
+		MenuOperacion(self.numero1, self.numero2, self.operacion)
+		
+		print ("los numeros recibidos son: ",self.numero1,"y", self.numero2, "y la operacion es: ", self.operacion)
+
+		if self.operacion == '1':
+			self.suman=socket1.recv(1024).decode("UTF-8")
+			print ("La suma es: ", self.suman)
+			self.request.send(self.suman.encode("UTF-8"))
+			
+		elif self.operacion == '2':
+			self.restan=socket2.recv(1024).decode("UTF-8")
+			print ("La resta es: ", self.restan)
+			self.request.send(self.restan.encode("UTF-8"))
+		else: pass
+
+
+		
+def main():
+	print ("Servidor intermedio escuchando...")
+	host2="localhost"
+	puerto=9999   #entre 0 y 10000, por los 9000 no estan usados
+
+	server1= socketserver.TCPServer((host2,puerto),miHandler)
+	print ("server corriendo")
+	server1.serve_forever()
+main()
